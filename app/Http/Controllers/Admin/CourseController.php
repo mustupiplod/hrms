d0e5d86993938f164ecacc9c445286683e448dad
mustupiplod <mustafa.piplod@gmail.com>
2020-02-22 16:17:26 +0530

    Course Controller add

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use App\Models\Course;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
@@ -11,6 +11,7 @@ class CourseController extends Controller
     public function index()
     {
         $courses = DB::table('courses')->get();
+        $course = Course::all();
     }
 
     public function create()
