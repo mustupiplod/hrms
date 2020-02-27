366ef6c9849c63b945968e2e5fc28f1902f760a6
mustupiplod <mustafa.piplod@gmail.com>
2020-02-27 13:38:00 +0530

    Events model added

---
+++ app/Events.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Events extends Model
+{
+    //
+//    protected $table = 'events';
+    protected $fillable = [
+        'title',
+        'color',
+        'start_date',
+        'end_date'
+        ];
+}
