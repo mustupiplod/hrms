d9787806c374b43c93b6fc160d533af63fcbc38a
mustupiplod <mustafa.piplod@gmail.com>
2020-02-27 13:34:02 +0530

    employee controller updated

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\controller;
+
+use App\Http\Controllers\Controller;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,41 +11,75 @@ class EmployeeController extends Controller
 {
     public function index()
     {
-        $employees = DB::table('employees')->get();
-        return view('admin.employee.index',['employees'=>$employees]);
+        $employees = DB::table('employees')
+            ->join('marital_statuses', 'marital_statuses.id', '=', 'employees.marital_status')
+            ->join('education_masters', 'education_masters.id', '=', 'employees.education')
+            ->join('degrees','degrees.id','=','employees.degree')
+            ->join('parent_departments', 'parent_departments.id', '=', 'employees.parent_department')
+            ->join('departments', 'departments.id', '=', 'employees.department')
+            ->join('designations', 'designations.id', '=', 'employees.designation')
+            ->select('employees.*', 'marital_statuses.marital_name', 'education_masters.school_name','degrees.degree_name', 'parent_departments.parent_depart_name', 'departments.department_name', 'designations.designation_name')
+            ->get();
+        return view('admin.employee.index', ['employees' => $employees]);
     }
+
     public function create()
     {
         $departments = DB::table('departments')->get();
         $designations = DB::table('designations')->get();
         $parent_departments = DB::table('parent_departments')->get();
+        $educations = DB::table('education_masters')->get();
         $degrees = DB::table('degrees')->get();
-//        $marital = DB::table('marital_status')
-        return view('admin.employee.create',['departments'=>$departments,'designations'=>$designations,'degrees'=>$degrees,'parent_departments'=>$parent_departments]);
+        $maritals = DB::table('marital_statuses')->get();
+        return view('admin.employee.create', ['departments' => $departments, 'designations' => $designations, 'educations'=>$educations,'degrees' => $degrees, 'parent_departments' => $parent_departments, 'maritals' => $maritals]);
     }
+
     public function store(Request $request)
     {
-        $employee = new Employee();
+//        $employee = new Employee();
         $employee = Employee::create($request->all());
         $employee->save();
         return redirect('/admin/employee');
     }
+
     public function show($id)
     {
-        $employee = Employee::findOrFail($id);
-        return(view('admin.employee.show',compact('employee')));
+//        $employee = Employee::findOrFail($id);
+        $employees = DB::table('employees')
+            ->join('marital_statuses', 'marital_statuses.id', '=', 'employees.marital_status')
+            ->join('education_masters', 'education_masters.id', '=', 'employees.education')
+            ->join('degrees','degrees.id','=','employees.degree')
+            ->join('parent_departments', 'parent_departments.id', '=', 'employees.parent_department')
+            ->join('departments', 'departments.id', '=', 'employees.department')
+            ->join('designations', 'designations.id', '=', 'employees.designation')
+            ->select('employees.*', 'marital_statuses.marital_name', 'education_masters.school_name','degrees.degree_name', 'parent_departments.parent_depart_name', 'departments.department_name', 'designations.designation_name')
+            ->where('employees.id', '=', $id)
+            ->get();
+//        dd($employees);
+        return (view('admin.employee.show', compact('employees')));
     }
+
     public function edit($id)
     {
         $departments = DB::table('departments')->get();
         $designations = DB::table('designations')->get();
         $parent_departments = DB::table('parent_departments')->get();
+        $educations = DB::table('education_masters')->get();
         $degrees = DB::table('degrees')->get();
-//        $marital = DB::table('marital_status')
-        $employee = Employee::whereId($id)->first();
-        return view('admin.employee.edit',['employee'=>$employee,'departments'=>$departments,'designations'=>$designations,'degrees'=>$degrees,'parent_departments'=>$parent_departments]);
-
+        $maritals = DB::table('marital_statuses')->get();
+        $employees = DB::table('employees')
+            ->join('marital_statuses', 'marital_statuses.id', '=', 'employees.marital_status')
+            ->join('education_masters', 'education_masters.id', '=', 'employees.education')
+            ->join('degrees','degrees.id','=','employees.degree')
+            ->join('parent_departments', 'parent_departments.id', '=', 'employees.parent_department')
+            ->join('departments', 'departments.id', '=', 'employees.department')
+            ->join('designations', 'designations.id', '=', 'employees.designation')
+            ->select('employees.*', 'marital_statuses.marital_name', 'education_masters.school_name','degrees.degree_name', 'parent_departments.parent_depart_name', 'departments.department_name', 'designations.designation_name')
+            ->where('employees.id', '=', $id)
+            ->get();
+        return view('admin.employee.edit',['employees'=>$employees,'departments'=>$departments,'designations'=>$designations,'educations'=>$educations,'degrees'=>$degrees,'parent_departments'=>$parent_departments,'maritals'=>$maritals]);
     }
+
     public function update(Request $request, $id)
     {
         $employee = Employee::findOrFail($id);
@@ -52,8 +87,11 @@ class EmployeeController extends Controller
         $employee->save();
         return redirect('/admin/employee');
     }
+
     public function delete($id)
     {
-
+        $employee = Employee::find($id);
+        $employee->delete();
+        return redirect('/admin/employee');
     }
 }
