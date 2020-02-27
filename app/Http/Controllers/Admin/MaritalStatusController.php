609bc8ec2f00170534e61da316db5f7444ec9e50
mustupiplod <mustafa.piplod@gmail.com>
2020-02-27 09:49:41 +0530

    marital status master controller edited

--- app/Http/Controllers/Admin/MaritalStatusController.php
+++ app/Http/Controllers/Admin/MaritalStatusController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\MaritalStatus;
