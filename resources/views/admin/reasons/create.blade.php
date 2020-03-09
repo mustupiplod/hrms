288d57b094aa6e0843e8f52dba4f51ec61a178fb
mustupiplod <mustafa.piplod@gmail.com>
2020-03-09 16:10:30 +0530

    Questionnaire master views files added

--- resources/views/admin/reasons/create.blade.php
+++ resources/views/admin/reasons/create.blade.php
@@ -10,11 +10,13 @@ $sec = Carbon::now()->second;
 $res_id = $year . "/" . $month . "/" . $day . "/" . $mins . "/" . $sec;
 ?>
 <head>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+
 </head>
 
 <div class="col-lg-8 offset-2">
