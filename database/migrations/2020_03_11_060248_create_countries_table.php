257f4bd85af84d6989300ca9add8247eedcf6a65
mustupiplod <mustafa.piplod@gmail.com>
2020-03-11 18:27:09 +0530

    Countries master miggration file added

---
+++ database/migrations/2020_03_11_060248_create_countries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->bigIncrements('country_id');
+            $table->string('country_name');
+            $table->integer('is_active')->nullable()->comment('0 means active | 1 means inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
